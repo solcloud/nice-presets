3aaba85a4211bea8eb5e1f92daaa449253e38c69
solcloud <74121353+solcloud@users.noreply.github.com>
2022-04-16 17:37:01 +0200

    rm deb

--- presets/raspi3b/target/etc/start_services.sh
+++ presets/raspi3b/target/etc/start_services.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-if [ -r /debootstrap/ ]; then
-    echo "Doing debootstrap second stage first install"
-    rm -rf /var/run && mkdir -p /var/run
-    /debootstrap/debootstrap --second-stage || cat /debootstrap/debootstrap.log
-    rm -rf /debootstrap/
-fi
-
 sleep 2 && /etc/init/network.sh usb0 &> /dev/null
 chmod 0600 /etc/ssh/ssh_host*
 mkdir -p /var/chroot/ssh
